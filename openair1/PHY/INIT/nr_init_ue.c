saffanazyan07/oai_w-agf

remove a useless copy and specific buffer for all UE UL payload

openair1/PHY/INIT/nr_init_ue.c
@@ -523,8 +523,7 @@ void free_nr_ue_ul_harq(NR_UL_UE_HARQ_t harq_list[NR_MAX_ULSCH_HARQ_PROCESSES], 
   }
 
   for (int i = 0; i < number_of_processes; i++) {
-    free_and_zero(harq_list[i].a);
-    free_and_zero(harq_list[i].b);
+    free_and_zero(harq_list[i].aPayload);
     for (int r = 0; r < a_segments; r++) {
       free_and_zero(harq_list[i].c[r]);
       free_and_zero(harq_list[i].d[r]);
@@ -587,13 +586,9 @@ void nr_init_ul_harq_processes(NR_UL_UE_HARQ_t harq_list[NR_MAX_ULSCH_HARQ_PROCE
 
     memset(harq_list + i, 0, sizeof(NR_UL_UE_HARQ_t));
 
-    harq_list[i].a = malloc16(ulsch_bytes);
-    DevAssert(harq_list[i].a);
-    bzero(harq_list[i].a, ulsch_bytes);
-
-    harq_list[i].b = malloc16(ulsch_bytes);
-    DevAssert(harq_list[i].b);
-    bzero(harq_list[i].b, ulsch_bytes);
+    harq_list[i].aPayload = malloc16(ulsch_bytes);
+    DevAssert(harq_list[i].aPayload);
+    bzero(harq_list[i].aPayload, ulsch_bytes);
 
     harq_list[i].c = malloc16(a_segments*sizeof(uint8_t *));
     harq_list[i].d = malloc16(a_segments*sizeof(uint16_t *));

openair1/SCHED_NR_UE/fapi_nr_ue_l1.c
@@ -469,7 +469,7 @@ static void nr_ue_scheduled_response_ul(PHY_VARS_NR_UE *phy, fapi_nr_ul_config_r
                 ul_config->slot,
                 pdu->pusch_config_pdu.tx_request_body.pdu_length,
                 current_harq_pid);
-          memcpy(harq_process_ul_ue->a,
+          memcpy(harq_process_ul_ue->aPayload,
                  pdu->pusch_config_pdu.tx_request_body.pdu,
                  pdu->pusch_config_pdu.tx_request_body.pdu_length);
         }

openair1/SIMULATION/NR_PHY/ulschsim.c
@@ -507,7 +507,7 @@ int main(int argc, char **argv)
   ulsch_ue->pusch_pdu.target_code_rate = code_rate;
   ulsch_ue->pusch_pdu.qam_mod_order = mod_order;
   ulsch_ue->pusch_pdu.ldpcBaseGraph = get_BG(TBS, code_rate);
-  unsigned char *test_input = harq_process_ul_ue->a;
+  unsigned char *test_input = harq_process_ul_ue->aPayload;
 
   ///////////
   ////////////////////////////////////////////////////////////////////////////////////////////

openair1/SIMULATION/NR_PHY/ulsim.c
@@ -1136,7 +1136,7 @@ int main(int argc, char *argv[])
         }
 
         for (int i = 0; i < (TBS / 8); i++)
-          UE->ul_harq_processes[harq_pid].a[i] = i & 0xff;
+          UE->ul_harq_processes[harq_pid].aPayload[i] = i & 0xff;
 
         if (input_fd == NULL) {
           // set FAPI parameters for UE, put them in the scheduled response and call
@@ -1452,7 +1452,7 @@ int main(int argc, char *argv[])
 
       for (i = 0; i < TBS; i++) {
         uint8_t estimated_output_bit = (ulsch_gNB->harq_process->b[i / 8] & (1 << (i & 7))) >> (i & 7);
-        uint8_t test_input_bit = (UE->ul_harq_processes[harq_pid].b[i / 8] & (1 << (i & 7))) >> (i & 7);
+        uint8_t test_input_bit = (UE->ul_harq_processes[harq_pid].aPayload[i / 8] & (1 << (i & 7))) >> (i & 7);
       
         if (estimated_output_bit != test_input_bit) {
           /*if(errors_decoding == 0)

gourav-prateek-sharma/edaf-openairinterface

openair1/PHY/INIT/nr_init_ue.c
@@ -534,8 +534,7 @@ void free_nr_ue_ul_harq(NR_UL_UE_HARQ_t harq_list[NR_MAX_ULSCH_HARQ_PROCESSES], 
   }
 
   for (int i = 0; i < number_of_processes; i++) {
-    free_and_zero(harq_list[i].a);
-    free_and_zero(harq_list[i].b);
+    free_and_zero(harq_list[i].payload_AB);
     for (int r = 0; r < a_segments; r++) {
       free_and_zero(harq_list[i].c[r]);
       free_and_zero(harq_list[i].d[r]);
@@ -598,13 +597,9 @@ void nr_init_ul_harq_processes(NR_UL_UE_HARQ_t harq_list[NR_MAX_ULSCH_HARQ_PROCE
 
     memset(harq_list + i, 0, sizeof(NR_UL_UE_HARQ_t));
 
-    harq_list[i].a = malloc16(ulsch_bytes);
-    DevAssert(harq_list[i].a);
-    bzero(harq_list[i].a, ulsch_bytes);
-
-    harq_list[i].b = malloc16(ulsch_bytes);
-    DevAssert(harq_list[i].b);
-    bzero(harq_list[i].b, ulsch_bytes);
+    harq_list[i].payload_AB = malloc16(ulsch_bytes);
+    DevAssert(harq_list[i].payload_AB);
+    bzero(harq_list[i].payload_AB, ulsch_bytes);
 
     harq_list[i].c = malloc16(a_segments*sizeof(uint8_t *));
     harq_list[i].d = malloc16(a_segments*sizeof(uint16_t *));

openair1/SCHED_NR_UE/fapi_nr_ue_l1.c
@@ -468,7 +468,7 @@ static void nr_ue_scheduled_response_ul(PHY_VARS_NR_UE *phy, fapi_nr_ul_config_r
                 ul_config->slot,
                 pdu->pusch_config_pdu.tx_request_body.pdu_length,
                 current_harq_pid);
-          memcpy(harq_process_ul_ue->a,
+          memcpy(harq_process_ul_ue->payload_AB,
                  pdu->pusch_config_pdu.tx_request_body.pdu,
                  pdu->pusch_config_pdu.tx_request_body.pdu_length);
         }

openair1/SIMULATION/NR_PHY/ulschsim.c
@@ -507,7 +507,7 @@ int main(int argc, char **argv)
   ulsch_ue->pusch_pdu.target_code_rate = code_rate;
   ulsch_ue->pusch_pdu.qam_mod_order = mod_order;
   ulsch_ue->pusch_pdu.ldpcBaseGraph = get_BG(TBS, code_rate);
-  unsigned char *test_input = harq_process_ul_ue->a;
+  unsigned char *test_input = harq_process_ul_ue->payload_AB;
 
   ///////////
   ////////////////////////////////////////////////////////////////////////////////////////////

openair1/SIMULATION/NR_PHY/ulsim.c
@@ -1141,7 +1141,7 @@ int main(int argc, char *argv[])
         }
 
         for (int i = 0; i < (TBS / 8); i++)
-          UE->ul_harq_processes[harq_pid].a[i] = i & 0xff;
+          UE->ul_harq_processes[harq_pid].payload_AB[i] = i & 0xff;
 
         if (input_fd == NULL) {
           // set FAPI parameters for UE, put them in the scheduled response and call
@@ -1457,8 +1457,8 @@ int main(int argc, char *argv[])
 
       for (i = 0; i < TBS; i++) {
         uint8_t estimated_output_bit = (ulsch_gNB->harq_process->b[i / 8] & (1 << (i & 7))) >> (i & 7);
-        uint8_t test_input_bit = (UE->ul_harq_processes[harq_pid].b[i / 8] & (1 << (i & 7))) >> (i & 7);
-      
+        uint8_t test_input_bit = (UE->ul_harq_processes[harq_pid].payload_AB[i / 8] & (1 << (i & 7))) >> (i & 7);
+
         if (estimated_output_bit != test_input_bit) {
           /*if(errors_decoding == 0)
               printf("\x1B[34m""[frame %d][trial %d]\t1st bit in error in decoding     = %d\n" "\x1B[0m", frame, trial, i);*/