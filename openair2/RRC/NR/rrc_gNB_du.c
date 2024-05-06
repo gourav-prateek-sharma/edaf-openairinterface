motangpuar/oai5g

gNB-DU config update msg def and handle in CU

openair2/RRC/NR/rrc_gNB_du.c
@@ -213,6 +213,100 @@ static int invalidate_du_connections(gNB_RRC_INST *rrc, sctp_assoc_t assoc_id)
   return count;
 }
 
+static void update_cell_info(nr_rrc_du_container_t *du, const f1ap_served_cell_info_t *new_ci)
+{
+  DevAssert(du != NULL);
+  DevAssert(new_ci != NULL);
+
+  AssertFatal(du->setup_req->num_cells_available == 1, "expected 1 cell for DU, but has %d\n", du->setup_req->num_cells_available);
+  f1ap_served_cell_info_t *ci = &du->setup_req->cell[0].info;
+
+  ci->nr_cellid = new_ci->nr_cellid;
+  ci->nr_pci = new_ci->nr_pci;
+  if (new_ci->tac != NULL)
+    *ci->tac = *new_ci->tac;
+  ci->num_ssi = new_ci->num_ssi;
+  ci->sst = new_ci->sst;
+  ci->sd = new_ci->sd;
+  ci->mode = new_ci->mode;
+  if (ci->mode == F1AP_MODE_TDD)
+    ci->tdd = new_ci->tdd;
+  else
+    ci->fdd = new_ci->fdd;
+}
+
+void rrc_gNB_process_f1_du_configuration_update(f1ap_gnb_du_configuration_update_t *conf_up, sctp_assoc_t assoc_id)
+{
+  AssertFatal(assoc_id != 0, "illegal assoc_id == 0: should be -1 (monolithic) or >0 (split)\n");
+  gNB_RRC_INST *rrc = RC.nrrrc[0];
+  DevAssert(rrc);
+
+  // check:
+  // - it is one cell
+  // - PLMN and Cell ID matches
+  // - no previous DU with the same ID
+  // else reject
+
+  nr_rrc_du_container_t *du = get_du_by_assoc_id(rrc, assoc_id);
+  AssertError(du != NULL, return, "no DU found for assoc_id %d\n", assoc_id);
+
+  const f1ap_served_cell_info_t *info = &du->setup_req->cell[0].info;
+  if(conf_up->num_cells_to_add > 0){
+    // Here we check if the number of cell limit is respectet, otherwise send failure
+    LOG_W(RRC, "du_configuration_update->cells_to_add_list is not supported yet");
+  }
+
+  if(conf_up->num_cells_to_modify > 0){
+    // here the old nrcgi is used to find the cell information, if it exist then we modify consequently otherwise we fail
+    AssertFatal(conf_up->num_cells_to_modify == 1, "cannot handle more than one cell!\n");
+
+    if (info->nr_cellid != conf_up->cell_to_modify[0].old_nr_cellid) {
+      LOG_W(RRC, "no cell with ID %ld found, ignoring gNB-DU configuration update\n", conf_up->cell_to_modify[0].old_nr_cellid);
+      return;
+    }
+
+    // verify the new plmn of the cell
+    if (!rrc_gNB_plmn_matches(rrc, &conf_up->cell_to_modify[0].info)) {
+      LOG_W(RRC, "PLMN does not match, ignoring gNB-DU configuration update\n");
+      return;
+    }
+
+    update_cell_info(du, &conf_up->cell_to_modify[0].info);
+
+    const f1ap_gnb_du_system_info_t *sys_info = conf_up->cell_to_modify[0].sys_info;
+
+    if (sys_info != NULL && sys_info->mib != NULL && !(sys_info->sib1 == NULL && get_softmodem_params()->sa)) {
+      // MIB is mandatory, so will be overwritten. SIB1 is optional, so will
+      // only be overwritten if present in sys_info
+      ASN_STRUCT_FREE(asn_DEF_NR_MIB, du->mib);
+      if (sys_info->sib1 != NULL)
+        ASN_STRUCT_FREE(asn_DEF_NR_SIB1, du->sib1);
+
+      NR_BCCH_BCH_Message_t *mib = NULL;
+      if (!extract_sys_info(sys_info, &mib, &du->sib1)) {
+        LOG_W(RRC, "cannot update sys_info for DU %ld\n", du->setup_req->gNB_DU_id);
+      } else {
+        DevAssert(mib != NULL);
+        du->mib = mib->message.choice.mib;
+        mib->message.choice.mib = NULL;
+        ASN_STRUCT_FREE(asn_DEF_NR_BCCH_BCH_MessageType, mib);
+        LOG_I(RRC, "update system information of DU %ld\n", du->setup_req->gNB_DU_id);
+      }
+    }
+  }
+
+  if(conf_up->num_cells_to_delete > 0){
+    // delete the cell and send cell to desactive IE in the response.
+    LOG_W(RRC, "du_configuration_update->cells_to_delete_list is not supported yet");
+  }
+
+
+  /* Send DU Configuration Acknowledgement */
+  f1ap_gnb_du_configuration_update_acknowledge_t ack = {.transaction_id = conf_up->transaction_id};
+
+  rrc->mac_rrc.gnb_du_configuration_update_acknowledge(assoc_id, &ack);
+}
+
 void rrc_CU_process_f1_lost_connection(gNB_RRC_INST *rrc, f1ap_lost_connection_t *lc, sctp_assoc_t assoc_id)
 {
   AssertFatal(assoc_id != 0, "illegal assoc_id == 0: should be -1 (monolithic) or >0 (split)\n");

gourav-prateek-sharma/edaf-openairinterface

openair2/RRC/NR/rrc_gNB_du.c
@@ -219,6 +219,99 @@ static int invalidate_du_connections(gNB_RRC_INST *rrc, sctp_assoc_t assoc_id)
   return count;
 }
 
+static void update_cell_info(nr_rrc_du_container_t *du, const f1ap_served_cell_info_t *new_ci)
+{
+  DevAssert(du != NULL);
+  DevAssert(new_ci != NULL);
+
+  AssertFatal(du->setup_req->num_cells_available == 1, "expected 1 cell for DU, but has %d\n", du->setup_req->num_cells_available);
+  f1ap_served_cell_info_t *ci = &du->setup_req->cell[0].info;
+
+  ci->nr_cellid = new_ci->nr_cellid;
+  ci->nr_pci = new_ci->nr_pci;
+  if (new_ci->tac != NULL)
+    *ci->tac = *new_ci->tac;
+  ci->num_ssi = new_ci->num_ssi;
+  ci->sst = new_ci->sst;
+  ci->sd = new_ci->sd;
+  ci->mode = new_ci->mode;
+  if (ci->mode == F1AP_MODE_TDD)
+    ci->tdd = new_ci->tdd;
+  else
+    ci->fdd = new_ci->fdd;
+}
+
+void rrc_gNB_process_f1_du_configuration_update(f1ap_gnb_du_configuration_update_t *conf_up, sctp_assoc_t assoc_id)
+{
+  AssertFatal(assoc_id != 0, "illegal assoc_id == 0: should be -1 (monolithic) or >0 (split)\n");
+  gNB_RRC_INST *rrc = RC.nrrrc[0];
+  DevAssert(rrc);
+
+  // check:
+  // - it is one cell
+  // - PLMN and Cell ID matches
+  // - no previous DU with the same ID
+  // else reject
+
+  nr_rrc_du_container_t *du = get_du_by_assoc_id(rrc, assoc_id);
+  AssertError(du != NULL, return, "no DU found for assoc_id %d\n", assoc_id);
+
+  const f1ap_served_cell_info_t *info = &du->setup_req->cell[0].info;
+  if (conf_up->num_cells_to_add > 0) {
+    // Here we check if the number of cell limit is respectet, otherwise send failure
+    LOG_W(RRC, "du_configuration_update->cells_to_add_list is not supported yet");
+  }
+
+  if (conf_up->num_cells_to_modify > 0) {
+    // here the old nrcgi is used to find the cell information, if it exist then we modify consequently otherwise we fail
+    AssertFatal(conf_up->num_cells_to_modify == 1, "cannot handle more than one cell!\n");
+
+    if (info->nr_cellid != conf_up->cell_to_modify[0].old_nr_cellid) {
+      LOG_W(RRC, "no cell with ID %ld found, ignoring gNB-DU configuration update\n", conf_up->cell_to_modify[0].old_nr_cellid);
+      return;
+    }
+
+    // verify the new plmn of the cell
+    if (!rrc_gNB_plmn_matches(rrc, &conf_up->cell_to_modify[0].info)) {
+      LOG_W(RRC, "PLMN does not match, ignoring gNB-DU configuration update\n");
+      return;
+    }
+
+    update_cell_info(du, &conf_up->cell_to_modify[0].info);
+
+    const f1ap_gnb_du_system_info_t *sys_info = conf_up->cell_to_modify[0].sys_info;
+
+    if (sys_info != NULL && sys_info->mib != NULL && !(sys_info->sib1 == NULL && get_softmodem_params()->sa)) {
+      // MIB is mandatory, so will be overwritten. SIB1 is optional, so will
+      // only be overwritten if present in sys_info
+      ASN_STRUCT_FREE(asn_DEF_NR_MIB, du->mib);
+      if (sys_info->sib1 != NULL)
+        ASN_STRUCT_FREE(asn_DEF_NR_SIB1, du->sib1);
+
+      NR_BCCH_BCH_Message_t *mib = NULL;
+      if (!extract_sys_info(sys_info, &mib, &du->sib1)) {
+        LOG_W(RRC, "cannot update sys_info for DU %ld\n", du->setup_req->gNB_DU_id);
+      } else {
+        DevAssert(mib != NULL);
+        du->mib = mib->message.choice.mib;
+        mib->message.choice.mib = NULL;
+        ASN_STRUCT_FREE(asn_DEF_NR_BCCH_BCH_MessageType, mib);
+        LOG_I(RRC, "update system information of DU %ld\n", du->setup_req->gNB_DU_id);
+      }
+    }
+  }
+
+  if (conf_up->num_cells_to_delete > 0) {
+    // delete the cell and send cell to desactive IE in the response.
+    LOG_W(RRC, "du_configuration_update->cells_to_delete_list is not supported yet");
+  }
+
+  /* Send DU Configuration Acknowledgement */
+  f1ap_gnb_du_configuration_update_acknowledge_t ack = {.transaction_id = conf_up->transaction_id};
+
+  rrc->mac_rrc.gnb_du_configuration_update_acknowledge(assoc_id, &ack);
+}
+
 void rrc_CU_process_f1_lost_connection(gNB_RRC_INST *rrc, f1ap_lost_connection_t *lc, sctp_assoc_t assoc_id)
 {
   AssertFatal(assoc_id != 0, "illegal assoc_id == 0: should be -1 (monolithic) or >0 (split)\n");